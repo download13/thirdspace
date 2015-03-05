fa60fff8e57f7d5b9f9ba651db5ba35a454292a5
Jonathan Boiser <jboiser@gmail.com>
2015-03-04 20:36:04 -0800

    Added a placeholder map for 'Find your space' feature.

--- website/app.py
+++ website/app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/findaspace', methods = ['GET'])
+def findaspace():
+	return render_template('findaspace.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
